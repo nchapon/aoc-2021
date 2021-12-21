e9f2650af687799c2ae4c5885e9362a8815a6860
nchapon <nchapon@gmail.com>
2021-12-21 13:53:51 +0100

    Day 3 - Refactoring

--- src/aoc_2021/day3.clj
+++ src/aoc_2021/day3.clj
@@ -1,7 +1,6 @@
 (ns aoc-2021.day3 
   (:require
-    [aoc-2021.core :refer [read-puzzle]]
-    [clojure.string :as string]))
+   [aoc-2021.core :refer [read-puzzle]]))
 
 (def gamma-rate (partial apply max-key val))
 (def epsilon-rate (partial apply min-key val))
@@ -35,60 +34,67 @@
      (compute-rate epsilon-rate input)
      ))
 
-
-
-(defn get-most-significant-bit
-  ""
+(defn count-bits
+  "Count bits for input at position"
   [input pos]
-  (let [counted-bits (get-bit-frequencies-by-position input pos)
-        zeros (get counted-bits "0")
-        ones (get counted-bits "1")]
-    (if (>= ones zeros) "1" "0")
+  (let [counted-bits (get-bit-frequencies-by-position input pos)]
+    [(get counted-bits "0") (get counted-bits "1")]
     ))
 
-(defn get-less-significant-bit
-  ""
-  [input pos]
-  (let [counted-bits (get-bit-frequencies-by-position input pos)
-        zeros (get counted-bits "0")
-        ones (get counted-bits "1")]
-    (if (<= zeros ones) "0" "1")
-    ))
+(defn most-common
+  "Returns most common numbers, 1 if equality"
+  [[zeros ones]]
+  (if (>= ones zeros) "1" "0"))
 
+(defn least-common
+  "Returns least common numbers, 0 if equality"
+  [[zeros ones]]
+  (if (<= zeros ones) "0" "1"))
 
-(defn number-filter
+(def most-common-bit (comp most-common count-bits))
+(def least-common-bit (comp least-common count-bits))
+
+(defn filter-numbers-by
   ""
-  [pos bit numbers]
+  [bit pos numbers]
   (if (= (subs numbers pos (inc pos)) bit)
     true false))
 
-(defn get-oxygen-generator-rating
-  ""
-  [input]
+(defn filter-by-bit-criteria
+  "Filter input numbers by bit criteria function"
+  [bit-criteria-fn input]
 
   (loop [numbers input
          pos 0]
     (if (= 1 (count numbers))
       numbers
-      (recur (filter #(number-filter pos (get-most-significant-bit numbers pos) %) numbers) (inc pos)))))
+      (recur (filter #(filter-numbers-by (bit-criteria-fn numbers pos) pos %) numbers) (inc pos)))))
+
+(defn get-oxygen-generator-rating
+  "Returns oxygen generator rating"
+  [input]
+
+  (->> input
+       (filter-by-bit-criteria most-common-bit)
+       (apply str)
+       (binary->int)))
 
 
 (defn get-co2-scrubber-rating
-  ""
+  "Returns CO2 scrubber rating"
   [input]
 
-  (loop [numbers input
-         pos 0]
-    (if (= 1 (count numbers))
-      numbers
-      (recur (filter #(number-filter pos (get-less-significant-bit numbers pos) %) numbers) (inc pos)))))
+  (->> input
+       (filter-by-bit-criteria least-common-bit)
+       (apply str)
+       (binary->int)))
 
 
 (defn part-2
   "Day 3 part 2"
   [input]
-  (* (binary->int (apply str (get-oxygen-generator-rating input)))
-     (binary->int (apply str (get-co2-scrubber-rating input)))))
+  (* (get-oxygen-generator-rating input)
+     (get-co2-scrubber-rating input)))
 
 
 (defn day3 []
